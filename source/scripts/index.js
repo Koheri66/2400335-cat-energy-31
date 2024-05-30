3eefdaefc0aa94f8960fd88cad9583ac9a14c6e2
Koheri66 <67858966+Koheri66@users.noreply.github.com>
2024-05-30 20:18:32 +0300

    Обновляет разметку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
-/* в этот файл добавляет скрипты*/
+// Menu
+
+const nav = document.querySelector('.nav');
+const navBtn = nav.querySelector('.nav__btn');
+
+// Menu no JS
+nav.classList.remove('nav--no-js');
+
+// Menu tooggle
+navBtn.onclick = function () {
+  nav.classList.toggle('nav--close');
+  nav.classList.toggle('nav--open');
+};
